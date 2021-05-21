1cc81bc11b96e2a44d48363dc0981f5f07b32f83
alonzorozco <sfabian.orozco@gmail.com>
2021-05-20 20:08:20 -0400

    add:patron4.2.0

--- patron4.rb
+++ patron4.rb
@@ -2,11 +2,11 @@ n = ARGV[0].to_i
 
 n.times do |i|
    if i % 3 == 0
-    print "1"
+      print "1"
    elsif i % 3 == 1
-    print "2"
+      print "2"
    else
-    print "3"
+      print "3"
    end
 end
 print "\n"
